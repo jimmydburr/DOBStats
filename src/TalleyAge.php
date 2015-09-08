9bda55d483951cb6203046a393f6ae751a01bfe3
Jimmy D. Burrell <jburrell@hostsignalz.com>
2015-09-07 20:55:12 -0500

    cleaned up

--- src/TalleyAge.php
+++ src/TalleyAge.php
@@ -7,32 +7,18 @@
  * @version 1.0
  * @author Jimmy Burrell
  */
+
 class TalleyAge
 {
-    /** @var int $age The driver's age. */
     protected $age = 0;
-    /** @var array $ageData The array which holds all driver ages. */
     protected $ageData = array();
-    /** @var array $ageUnder25 The array which holds driver ages under 25. */
     protected $ageUnder25 = array();
-    /** @var array $age25to34 The array which holds driver ages 25 to 34. */
     protected $age25to34 = array();
-    /** @var array $age35to44 The array which holds driver ages 35 to 44. */
     protected $age35to44 = array();
-    /** @var array $age45to54 The array which holds driver ages 45 to 54. */
     protected $age45to54 = array();
-    /** @var array $age55to64 The array which holds driver ages 55 to 64. */
     protected $age55to64 = array();
-    /** @var array $age65to74 The array which holds driver ages 65 to 74. */
     protected $age65to74 = array();
-    /** @var array $age75AndOver The array which holds driver ages 75 and over. */
     protected $age75AndOver = array();
-
-
-    public function __construct()
-	{
-	}
-
 
     public function tallyAge($age)
     {
@@ -41,4 +27,5 @@ class TalleyAge
         echo "array ageData count = " . count($ageData) . "\n";
         return count($ageData);
     }
+
 }
