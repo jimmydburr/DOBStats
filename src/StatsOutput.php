91190ac7e279f04260854b45d0b36f1a4bb09c7d
Jimmy D. Burrell <jburrell@hostsignalz.com>
2015-09-20 08:20:14 -0500

    clean up old code in TalleyAge and refactored StatusOutput to use new TalleyAge calling format

--- src/StatsOutput.php
+++ src/StatsOutput.php
@@ -14,50 +14,50 @@ class StatsOutput
 	{
 		echo sprintf(
 			'Average driver age is %d based on a sampling of %d drivers.',
-			$ageTalley->getAverageAge("Total"),
-			$ageTalley->getDriverCounts("Total")
+			$ageTalley->getAverageAge("ageageData"),
+			$ageTalley->getDriverCounts("ageageData")
 			) . '<br />' . PHP_EOL;
 		echo sprintf(
 			'Drivers under age 25 total = %d with an average age of %d. These driver represent %3.1d percent of this driver sampling.',
-			$ageTalley->getDriverCounts("Under25"),
-			$ageTalley->getAverageAge("Under25"),
-			$ageTalley->getPercentOfDriverCategory("Under25")
+			$ageTalley->getDriverCounts("ageageUnder25"),
+			$ageTalley->getAverageAge("ageageUnder25"),
+			$ageTalley->getPercentOfDriverCategory("ageageUnder25")
 		) . '<br />' . PHP_EOL;
 		echo sprintf(
 			'Drivers age 25 to 34 total = %d with an average age of %d. These driver represent %3.4s percent of this driver sampling.',
-			$ageTalley->getDriverCounts("25to34"),
-			$ageTalley->getAverageAge("25to34"),
-			$ageTalley->getPercentOfDriverCategory("25to34")
+			$ageTalley->getDriverCounts("ageage25to34"),
+			$ageTalley->getAverageAge("ageage25to34"),
+			$ageTalley->getPercentOfDriverCategory("ageage25to34")
 		) . '<br />' . PHP_EOL;
 		echo sprintf(
 			'Drivers age 35 to 44 total = %d with an average age of %d. These driver represent %3.4s percent of this driver sampling.',
-			$ageTalley->getDriverCounts("35to44"),
-			$ageTalley->getAverageAge("35to44"),
-			$ageTalley->getPercentOfDriverCategory("35to44")
+			$ageTalley->getDriverCounts("age35to44"),
+			$ageTalley->getAverageAge("age35to44"),
+			$ageTalley->getPercentOfDriverCategory("age35to44")
 		) . '<br />' . PHP_EOL;
 		echo sprintf(
 			'Drivers age 45 to 54 total = %d with an average age of %d. These driver represent %3.4s percent of this driver sampling.',
-			$ageTalley->getDriverCounts("45to54"),
-			$ageTalley->getAverageAge("45to54"),
-			$ageTalley->getPercentOfDriverCategory("45to54")
+			$ageTalley->getDriverCounts("age45to54"),
+			$ageTalley->getAverageAge("age45to54"),
+			$ageTalley->getPercentOfDriverCategory("age45to54")
 		) . '<br />' . PHP_EOL;
 		echo sprintf(
 			'Drivers age 55 to 64 total = %d with an average age of %d. These driver represent %3.4s percent of this driver sampling.',
-			$ageTalley->getDriverCounts("55to64"),
-			$ageTalley->getAverageAge("55to64"),
-			$ageTalley->getPercentOfDriverCategory("55to64")
+			$ageTalley->getDriverCounts("age55to64"),
+			$ageTalley->getAverageAge("age55to64"),
+			$ageTalley->getPercentOfDriverCategory("age55to64")
 		) . '<br />' . PHP_EOL;
 		echo sprintf(
 			'Drivers age 65 to 74 total = %d with an average age of %d. These driver represent %3.4s percent of this driver sampling.',
-			$ageTalley->getDriverCounts("65to74"),
-			$ageTalley->getAverageAge("65to74"),
-			$ageTalley->getPercentOfDriverCategory("65to74")
+			$ageTalley->getDriverCounts("age65to74"),
+			$ageTalley->getAverageAge("age65to74"),
+			$ageTalley->getPercentOfDriverCategory("age65to74")
 		) . '<br />' . PHP_EOL;
 		echo sprintf(
 			'Drivers age 75 and over total = %d with an average age of %d. These driver represent %3.4s percent of this driver sampling.',
-			$ageTalley->getDriverCounts("75AndOver"),
-			$ageTalley->getAverageAge("75AndOver"),
-			$ageTalley->getPercentOfDriverCategory("75AndOver")
+			$ageTalley->getDriverCounts("age75AndOver"),
+			$ageTalley->getAverageAge("age75AndOver"),
+			$ageTalley->getPercentOfDriverCategory("age75AndOver")
 		) . '<br />' . PHP_EOL;
 	}	// end cliOutput
 }
